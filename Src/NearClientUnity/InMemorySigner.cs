e300bb8efadba6909af53893e85629b1c082ebc8
Sergei Siruk <sergei.siruk@gmail.com>
2019-12-10 11:40:00 +0200

    Base implementation InMemorySigner.

---
+++ Src/NearClientUnity/InMemorySigner.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Threading.Tasks;
+using NearClientUnity.KeyStores;
+using NearClientUnity.Utilities;
+
+namespace NearClientUnity
+{
+    public class InMemorySigner : Signer
+    {
+        private readonly KeyStore _keyStore;
+
+        public InMemorySigner(KeyStore keyStore)
+        {
+            _keyStore = keyStore;
+        }
+        public override async Task<PublicKey> CreateKeyAsync(string accountId, string networkId = "")
+        {
+            var keyPair = KeyPair.FromRandom("ed25519");
+            await _keyStore.SetKeyAsync(networkId, accountId, keyPair);
+            return keyPair.GetPublicKey();
+        }
+
+        public override async Task<PublicKey> GetPublicKeyAsync(string accountId = "", string networkId = "")
+        {
+            var keyPair = await _keyStore.GetKeyAsync(networkId, accountId);
+            return keyPair?.GetPublicKey();
+        }
+
+        public override async Task<Utilities.Signature> SignHashAsync(byte[] hash, string accountId = "", string networkId = "")
+        {
+            if (string.IsNullOrEmpty(accountId) || string.IsNullOrWhiteSpace(accountId))
+            {
+                throw new ArgumentException("InMemorySigner requires provided account id");
+            }
+            var keyPair = await _keyStore.GetKeyAsync(networkId, accountId);
+            if (keyPair == null)
+            {
+                throw new Exception($"Key for { accountId} not found in { networkId}");
+            }
+            return keyPair.Sign(hash);
+        }
+    }
+}
---
+++ Src/NearClientUnity/Signer.cs
@@ -0,0 +1,48 @@
+﻿using System.Security.Cryptography;
+using System.Threading.Tasks;
+using NearClientUnity.Utilities;
+
+namespace NearClientUnity
+{
+    public abstract class Signer
+    {
+        /**
+        * Creates new key and returns public key.
+        */
+        public abstract Task<PublicKey> CreateKeyAsync(string accountId, string networkId = "");
+
+        /**
+         * Returns public key for given account / network.
+         * @param accountId accountId to retrieve from.
+         * @param networkId network for this accountId.
+         */
+        public abstract Task<PublicKey> GetPublicKeyAsync(string accountId = "", string networkId = "");
+
+        /**
+         * Signs given hash.
+         * @param hash hash to sign.
+         * @param accountId accountId to use for signing.
+         * @param networkId network for this accontId.
+         */
+        public abstract Task<Utilities.Signature> SignHashAsync(byte[] hash, string accountId = "", string networkId = "");
+
+        /**
+         * Signs given message, by first hashing with sha256.
+         * @param message message to sign.
+         * @param accountId accountId to use for signing.
+         * @param networkId network for this accontId.
+         */
+        public async Task<Utilities.Signature> SignMessageAsync(byte[] message, string accountId = "", string networkId = "")
+        {
+            byte[] messageSha256;
+
+            using (var sha256 = SHA256.Create())
+            {
+                messageSha256 = sha256.ComputeHash(message);
+            }
+
+            var result = await SignHashAsync(messageSha256, accountId, networkId);
+            return result;
+        }
+}
+}
