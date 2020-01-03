daf86369c7fe756299f2da13ac704cd7058cc633
Viktor Siruk <vs@prezentor.com>
2020-01-03 10:51:18 +0200

    KeyPair ItShouldSignAndVerify is done

--- Src/NearClientUnityTests/Utils/KeyPairTests.cs
+++ Src/NearClientUnityTests/Utils/KeyPairTests.cs
@@ -1,6 +1,7 @@
 ﻿using NUnit.Framework;
-using System;
 using NearClientUnity.Utilities;
+using System.Text;
+using System.Security.Cryptography;
 
 namespace NearClientUnityTests.Utils
 {
@@ -10,9 +11,17 @@ namespace NearClientUnityTests.Utils
         [Test]
         public void ItShouldSignAndVerify()
         {
-            var keypair = new KeyPairEd25519(secretKey:"26x56YPzPDro5t2smQfGcYAPy3j7R2jB2NUb7xKbAGK23B6x4WNQPh3twb6oDksFov5X8ts5CtntUNbpQpAKFdbR");
-            Console.WriteLine("GetPublicKey - " + keypair.GetPublicKey());
+            var keypair = new KeyPairEd25519(secretKey:"26x56YPzPDro5t2smQfGcYAPy3j7R2jB2NUb7xKbAGK23B6x4WNQPh3twb6oDksFov5X8ts5CtntUNbpQpAKFdbR");            
             Assert.AreEqual(keypair.GetPublicKey().ToString(), "ed25519:AYWv9RAN1hpSQA4p1DLhCNnpnNXwxhfH9qeHN8B4nJ59");
+            byte[] messageBytes = Encoding.Default.GetBytes("message");
+            byte[] messageSha256;
+            using (var sha256 = SHA256.Create())
+            {
+                messageSha256 = sha256.ComputeHash(messageBytes);
+            }
+            var signature = keypair.Sign(messageSha256);
+            var signedMessage = Base58.Encode(signature.SignatureBytes);
+            Assert.AreEqual(signedMessage, "26gFr4xth7W9K7HPWAxq3BLsua8oTy378mC1MYFiEXHBBpeBjP8WmJEJo8XTBowetvqbRshcQEtBUdwQcAqDyP8T");
         }
     }    
 }
