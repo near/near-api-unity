8717c7425064307ff9ee9786afabddb5e7ae3fe6
Viktor Siruk <vs@prezentor.com>
2020-01-03 11:09:11 +0200

    KeyPair ItShouldSignAndVerifyWithRandom is done

--- Src/NearClientUnityTests/Utils/KeyPairTests.cs
+++ Src/NearClientUnityTests/Utils/KeyPairTests.cs
@@ -11,7 +11,7 @@ namespace NearClientUnityTests.Utils
         [Test]
         public void ItShouldSignAndVerify()
         {
-            var keypair = new KeyPairEd25519(secretKey:"26x56YPzPDro5t2smQfGcYAPy3j7R2jB2NUb7xKbAGK23B6x4WNQPh3twb6oDksFov5X8ts5CtntUNbpQpAKFdbR");            
+            var keypair = new KeyPairEd25519(secretKey: "26x56YPzPDro5t2smQfGcYAPy3j7R2jB2NUb7xKbAGK23B6x4WNQPh3twb6oDksFov5X8ts5CtntUNbpQpAKFdbR");
             Assert.AreEqual(keypair.GetPublicKey().ToString(), "ed25519:AYWv9RAN1hpSQA4p1DLhCNnpnNXwxhfH9qeHN8B4nJ59");
             byte[] messageBytes = Encoding.Default.GetBytes("message");
             byte[] messageSha256;
@@ -23,5 +23,19 @@ namespace NearClientUnityTests.Utils
             var signedMessage = Base58.Encode(signature.SignatureBytes);
             Assert.AreEqual(signedMessage, "26gFr4xth7W9K7HPWAxq3BLsua8oTy378mC1MYFiEXHBBpeBjP8WmJEJo8XTBowetvqbRshcQEtBUdwQcAqDyP8T");
         }
-    }    
+
+        [Test]
+        public void ItShouldSignAndVerifyWithRandom()
+        {
+            var keypair = KeyPairEd25519.FromRandom();
+            byte[] messageBytes = Encoding.Default.GetBytes("message");
+            byte[] messageSha256;
+            using (var sha256 = SHA256.Create())
+            {
+                messageSha256 = sha256.ComputeHash(messageBytes);
+            }
+            var signature = keypair.Sign(messageSha256);
+            Assert.AreEqual(keypair.Verify(messageSha256, signature.SignatureBytes), true);
+        }
+    }
 }
