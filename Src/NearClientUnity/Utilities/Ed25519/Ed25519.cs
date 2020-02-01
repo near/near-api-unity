5cebbaefa9fd9ba039db4d904c874b2cfc61a06b
Sergei Siruk <sergei.siruk@gmail.com>
2020-02-01 13:20:33 +0200

    original curve25519

--- Src/NearClientUnity/Utilities/Ed25519/Ed25519.cs
+++ Src/NearClientUnity/Utilities/Ed25519/Ed25519.cs
@@ -1,42 +1,114 @@
-﻿using NearClientUnity.Utilities.Ed25519.Internal.Ed25519Ref10.Ed25519Ops;
-using System;
+﻿using System;
+using Chaos.NaCl.Internal.Ed25519Ref10;
+using System.Diagnostics.Contracts;
 
-namespace NearClientUnity.Utilities.Ed25519
+namespace Chaos.NaCl
 {
     public static class Ed25519
     {
+        /// <summary>
+        /// Public Keys are 32 byte values. All possible values of this size a valid.
+        /// </summary>
+        public const int PublicKeySize = 32;
+        /// <summary>
+        /// Signatures are 64 byte values
+        /// </summary>
+        public const int SignatureSize = 64;
+        /// <summary>
+        /// Private key seeds are 32 byte arbitrary values. This is the form that should be generated and stored.
+        /// </summary>
+        public const int PrivateKeySeedSize = 32;
         /// <summary>
         /// A 64 byte expanded form of private key. This form is used internally to improve performance
         /// </summary>
         public const int ExpandedPrivateKeySize = 32 * 2;
 
         /// <summary>
-        /// Private key seeds are 32 byte arbitrary values. This is the form that should be
-        /// generated and stored.
+        /// Verify Ed25519 signature
         /// </summary>
-        public const int PrivateKeySeedSize = 32;
+        /// <param name="signature">Signature bytes</param>
+        /// <param name="message">Message</param>
+        /// <param name="publicKey">Public key</param>
+        /// <returns>True if signature is valid, false if it's not</returns>
+        public static bool Verify(ArraySegment<byte> signature, ArraySegment<byte> message, ArraySegment<byte> publicKey)
+        {
+            Contract.Requires<ArgumentException>(signature.Count == SignatureSize && publicKey.Count == PublicKeySize);
+
+            return Ed25519Operations.crypto_sign_verify(signature.Array, signature.Offset, message.Array, message.Offset, message.Count, publicKey.Array, publicKey.Offset);
+        }
 
         /// <summary>
-        /// Public Keys are 32 byte values. All possible values of this size a valid.
+        /// Verify Ed25519 signature
         /// </summary>
-        public const int PublicKeySize = 32;
+        /// <param name="signature">Signature bytes</param>
+        /// <param name="message">Message</param>
+        /// <param name="publicKey">Public key</param>
+        /// <returns>True if signature is valid, false if it's not</returns>
+        public static bool Verify(byte[] signature, byte[] message, byte[] publicKey)
+        {
+            Contract.Requires<ArgumentNullException>(signature != null && message != null && publicKey != null);
+            Contract.Requires<ArgumentException>(signature.Length == SignatureSize && publicKey.Length == PublicKeySize);
+
+            return Ed25519Operations.crypto_sign_verify(signature, 0, message, 0, message.Length, publicKey, 0);
+        }
 
         /// <summary>
-        /// Signatures are 64 byte values
+        /// Create new Ed25519 signature
         /// </summary>
-        public const int SignatureSize = 64;
+        /// <param name="signature">Buffer for signature</param>
+        /// <param name="message">Message bytes</param>
+        /// <param name="expandedPrivateKey">Expanded form of private key</param>
+        public static void Sign(ArraySegment<byte> signature, ArraySegment<byte> message, ArraySegment<byte> expandedPrivateKey)
+        {
+            Contract.Requires<ArgumentNullException>(signature.Array != null && message.Array != null && expandedPrivateKey.Array != null);
+            Contract.Requires<ArgumentException>(expandedPrivateKey.Count == ExpandedPrivateKeySize);
+
+            Ed25519Operations.crypto_sign(signature.Array, signature.Offset, message.Array, message.Offset, message.Count, expandedPrivateKey.Array, expandedPrivateKey.Offset);
+        }
+
+        /// <summary>
+        /// Create new Ed25519 signature
+        /// </summary>
+        /// <param name="signature">Buffer for signature</param>
+        /// <param name="message">Message bytes</param>
+        /// <param name="expandedPrivateKey">Expanded form of private key</param>
+        public static byte[] Sign(byte[] message, byte[] expandedPrivateKey)
+        {
+            Contract.Requires<ArgumentNullException>(message != null && expandedPrivateKey != null);
+            Contract.Requires<ArgumentException>(expandedPrivateKey.Length == ExpandedPrivateKeySize);
+
+            var signature = new byte[SignatureSize];
+            Sign(new ArraySegment<byte>(signature), new ArraySegment<byte>(message), new ArraySegment<byte>(expandedPrivateKey));
+            return signature;
+        }
+
+        /// <summary>
+        /// Calculate public key from private key seed
+        /// </summary>
+        /// <param name="privateKeySeed">Private key seed value</param>
+        /// <returns></returns>
+        public static byte[] PublicKeyFromSeed(byte[] privateKeySeed)
+        {
+            Contract.Requires<ArgumentNullException>(privateKeySeed != null);
+            Contract.Requires<ArgumentException>(privateKeySeed.Length == PrivateKeySeedSize);
+
+            byte[] privateKey;
+            byte[] publicKey;
+            KeyPairFromSeed(out publicKey, out privateKey, privateKeySeed);
+            CryptoBytes.Wipe(privateKey);
+            return publicKey;
+        }
 
         /// <summary>
         /// Calculate expanded form of private key from the key seed.
         /// </summary>
-        /// <param name="privateKeySeed">
-        /// Private key seed value
-        /// </param>
-        /// <returns>
-        /// Expanded form of the private key
-        /// </returns>
+        /// <param name="privateKeySeed">Private key seed value</param>
+        /// <returns>Expanded form of the private key</returns>
         public static byte[] ExpandedPrivateKeyFromSeed(byte[] privateKeySeed)
         {
+            Contract.Requires<ArgumentNullException>(privateKeySeed != null);
+            Contract.Requires<ArgumentException>(privateKeySeed.Length == PrivateKeySeedSize);
+
             byte[] privateKey;
             byte[] publicKey;
             KeyPairFromSeed(out publicKey, out privateKey, privateKeySeed);
@@ -47,21 +119,18 @@ namespace NearClientUnity.Utilities.Ed25519
         /// <summary>
         /// Calculate key pair from the key seed.
         /// </summary>
-        /// <param name="publicKey">
-        /// Public key
-        /// </param>
-        /// <param name="expandedPrivateKey">
-        /// Expanded form of the private key
-        /// </param>
-        /// <param name="privateKeySeed">
-        /// Private key seed value
-        /// </param>
+        /// <param name="publicKey">Public key</param>
+        /// <param name="expandedPrivateKey">Expanded form of the private key</param>
+        /// <param name="privateKeySeed">Private key seed value</param>
         public static void KeyPairFromSeed(out byte[] publicKey, out byte[] expandedPrivateKey, byte[] privateKeySeed)
         {
+            Contract.Requires<ArgumentNullException>(privateKeySeed != null);
+            Contract.Requires<ArgumentException>(privateKeySeed.Length == PrivateKeySeedSize);
+
             var pk = new byte[PublicKeySize];
             var sk = new byte[ExpandedPrivateKeySize];
 
-            Ed25519Operations.CryptoSignKeyPair(pk, 0, sk, 0, privateKeySeed, 0);
+            Ed25519Operations.crypto_sign_keypair(pk, 0, sk, 0, privateKeySeed, 0);
             publicKey = pk;
             expandedPrivateKey = sk;
         }
@@ -69,117 +138,19 @@ namespace NearClientUnity.Utilities.Ed25519
         /// <summary>
         /// Calculate key pair from the key seed.
         /// </summary>
-        /// <param name="publicKey">
-        /// Public key
-        /// </param>
-        /// <param name="expandedPrivateKey">
-        /// Expanded form of the private key
-        /// </param>
-        /// <param name="privateKeySeed">
-        /// Private key seed value
-        /// </param>
-        public static void KeyPairFromSeed(ArraySegment<byte> publicKey, ArraySegment<byte> expandedPrivateKey,
-            ArraySegment<byte> privateKeySeed)
+        /// <param name="publicKey">Public key</param>
+        /// <param name="expandedPrivateKey">Expanded form of the private key</param>
+        /// <param name="privateKeySeed">Private key seed value</param>
+        public static void KeyPairFromSeed(ArraySegment<byte> publicKey, ArraySegment<byte> expandedPrivateKey, ArraySegment<byte> privateKeySeed)
         {
-            Ed25519Operations.CryptoSignKeyPair(
+            Contract.Requires<ArgumentNullException>(publicKey.Array != null && expandedPrivateKey.Array != null && privateKeySeed.Array != null);
+            Contract.Requires<ArgumentException>(expandedPrivateKey.Count == ExpandedPrivateKeySize && privateKeySeed.Count == PrivateKeySeedSize);
+            Contract.Requires<ArgumentException>(publicKey.Count == PublicKeySize);
+
+            Ed25519Operations.crypto_sign_keypair(
                 publicKey.Array, publicKey.Offset,
                 expandedPrivateKey.Array, expandedPrivateKey.Offset,
                 privateKeySeed.Array, privateKeySeed.Offset);
-        }
-
-        /// <summary>
-        /// Calculate public key from private key seed
-        /// </summary>
-        /// <param name="privateKeySeed">
-        /// Private key seed value
-        /// </param>
-        /// <returns>
-        /// </returns>
-        public static byte[] PublicKeyFromSeed(byte[] privateKeySeed)
-        {
-            byte[] privateKey;
-            byte[] publicKey;
-            KeyPairFromSeed(out publicKey, out privateKey, privateKeySeed);
-            CryptoBytes.Wipe(privateKey);
-            return publicKey;
-        }
-
-        /// <summary>
-        /// Create new Ed25519 signature
-        /// </summary>
-        /// <param name="signature">
-        /// Buffer for signature
-        /// </param>
-        /// <param name="message">
-        /// Message bytes
-        /// </param>
-        /// <param name="expandedPrivateKey">
-        /// Expanded form of private key
-        /// </param>
-        public static void Sign(ArraySegment<byte> signature, ArraySegment<byte> message,
-            ArraySegment<byte> expandedPrivateKey)
-        {
-            Ed25519Operations.CryptoSign(signature.Array, signature.Offset, message.Array, message.Offset,
-                message.Count, expandedPrivateKey.Array, expandedPrivateKey.Offset);
-        }
-
-        /// <summary>
-        /// Create new Ed25519 signature
-        /// </summary>
-        /// <param name="message">
-        /// Message bytes
-        /// </param>
-        /// <param name="expandedPrivateKey">
-        /// Expanded form of private key
-        /// </param>
-        public static byte[] Sign(byte[] message, byte[] expandedPrivateKey)
-        {
-            var signature = new byte[SignatureSize];
-            Sign(new ArraySegment<byte>(signature), new ArraySegment<byte>(message),
-                new ArraySegment<byte>(expandedPrivateKey));
-            return signature;
-        }
-
-        /// <summary>
-        /// Verify Ed25519 signature
-        /// </summary>
-        /// <param name="signature">
-        /// Signature bytes
-        /// </param>
-        /// <param name="message">
-        /// Message
-        /// </param>
-        /// <param name="publicKey">
-        /// Public key
-        /// </param>
-        /// <returns>
-        /// True if signature is valid, false if it's not
-        /// </returns>
-        public static bool Verify(ArraySegment<byte> signature, ArraySegment<byte> message,
-            ArraySegment<byte> publicKey)
-        {
-            return Ed25519Operations.CryptoSignVerify(signature.Array, signature.Offset, message.Array,
-                message.Offset, message.Count, publicKey.Array, publicKey.Offset);
-        }
-
-        /// <summary>
-        /// Verify Ed25519 signature
-        /// </summary>
-        /// <param name="signature">
-        /// Signature bytes
-        /// </param>
-        /// <param name="message">
-        /// Message
-        /// </param>
-        /// <param name="publicKey">
-        /// Public key
-        /// </param>
-        /// <returns>
-        /// True if signature is valid, false if it's not
-        /// </returns>
-        public static bool Verify(byte[] signature, byte[] message, byte[] publicKey)
-        {
-            return Ed25519Operations.CryptoSignVerify(signature, 0, message, 0, message.Length, publicKey, 0);
         }
     }
 }
